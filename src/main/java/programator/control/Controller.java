81882821372c3c41b52f73aee1df1b27d1ea6c9d
vitalispopoff <54749416+vitalispopoff@users.noreply.github.com>
2020-02-20 21:34:47 +0100

    Update Controller.java

--- src/main/java/programator/control/Controller.java
+++ src/main/java/programator/control/Controller.java
@@ -1,22 +1,18 @@
 package programator.control;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static fi.iki.elonen.*;
+import static programator.Recoverable.serializeIt;
+import java.io.Serializable;
+import java.util.*;
+
+import com.fasterxml.jackson.*;
 import programator.betPool.TicketPool;
 import programator.types.Ticket;
-
-import java.io.Serializable;
-import java.util.List;
-import java.util.Map;
-
-import static fi.iki.elonen.NanoHTTPD.*;
-import static fi.iki.elonen.NanoHTTPD.Response.Status.*;
-import static programator.Recoverable.serializeIt;
 
 public class Controller implements Serializable {
 
     private final static String TICKET_ID_PARAMETER_NAME = "ticketId";
-    private /*static*/ TicketPool ticketPool = new TicketPool();
+    private TicketPool ticketPool = new TicketPool();
 
     public TicketPool getTicketPool() {
         return ticketPool;
@@ -27,24 +23,21 @@ public class Controller implements Serializable {
     }
 
     public Response serveAddTicketRequest(IHTTPSession session) {
-
         ObjectMapper objectMapper = new ObjectMapper();
         long theTicketId = System.currentTimeMillis();
-
         String lengthHeader = session.getHeaders().get("content-length");
         int contentLength = Integer.parseInt(lengthHeader);
         byte[] buffer = new byte[contentLength];
 
         try {
             session.getInputStream().read(buffer, 0, contentLength);
+            
             String requestBody = new String(buffer).trim();
             Ticket requestTicket = objectMapper.readValue(requestBody, Ticket.class);
+            
             requestTicket.setTicketId(theTicketId);
-
             ticketPool.addTicket(requestTicket, requestTicket.getBettor(), requestTicket.getAgent());
-
             serializeIt();
-
         } catch (Exception e) {
             System.err.println("Error in the request process:\n" + e);
             return newFixedLengthResponse(
@@ -52,7 +45,6 @@ public class Controller implements Serializable {
                     "text/plain",
                     "Internal error, the ticket is NOT in the pool.");
         }
-
         return newFixedLengthResponse(
                 OK,
                 "text/plain",
@@ -60,7 +52,6 @@ public class Controller implements Serializable {
     }
 
     public Response serveGetAllTicketsRequest(IHTTPSession session) {
-
         ObjectMapper objectMapper = new ObjectMapper();
         String response = "";
 
@@ -71,8 +62,7 @@ public class Controller implements Serializable {
             return newFixedLengthResponse(
                     INTERNAL_ERROR,
                     "text/plain",
-                    "Internal error, can't read all the tickets, not at once"
-            );
+                    "Internal error, can't read all the tickets, not at once");
         }
         return newFixedLengthResponse(
                 OK,
@@ -81,7 +71,6 @@ public class Controller implements Serializable {
     }
 
     public Response serveGetTicketRequest(IHTTPSession session) {
-
         Map<String, List<String>> requestParameters = session.getParameters();
 
         if (requestParameters.containsKey(TICKET_ID_PARAMETER_NAME)) {
@@ -97,37 +86,29 @@ public class Controller implements Serializable {
                         "text/plain",
                         "Request parameter 'ticketId' gotta be a number");
             }
-
             Ticket ticket = ticketPool.getTicket(ticketId);
             if (ticket != null) {
                 try {
                     ObjectMapper objectMapper = new ObjectMapper();
                     String response = objectMapper.writeValueAsString(ticket);
-
-
                     return newFixedLengthResponse(
                             OK,
                             "application/json",
                             response);
-
                 } catch (JsonProcessingException e) {
                     System.err.println("Error in request process:\n" + e);
                     return newFixedLengthResponse(
                             INTERNAL_ERROR,
                             "text/plain",
-                            "Internal error: can't get all the tickets"
-                    );
+                            "Internal error: can't get all the tickets");
                 }
             }
-
             return newFixedLengthResponse(NOT_FOUND, "application/json", "¯\\_(ツ)_/¯");
         }
-
         return newFixedLengthResponse(BAD_REQUEST, "text/plain", "Uncorrected request params");
     }
 
     public Response serveCheckTicketRequest(IHTTPSession session) {
-
         Map<String, List<String>> requestParameters = session.getParameters();
 
         if (requestParameters.containsKey(TICKET_ID_PARAMETER_NAME)) {
@@ -141,29 +122,23 @@ public class Controller implements Serializable {
                 return newFixedLengthResponse(
                         BAD_REQUEST,
                         "text/plain",
-                        "Request parameter 'ticketId' gotta be a number"
-                );
+                        "Request parameter 'ticketId' gotta be a number");
             }
-
             Ticket ticket = ticketPool.getTicket(ticketId);
             if (ticket != null) {
                 try {
                     ticketPool.checkTicket(ticketId);
-
                     serializeIt();
-
                     return newFixedLengthResponse(
                             OK,
                             "application/json",
                             "Ticket " + ticketId + " has been checked. You have successfully failed.");
-
                 } catch (Exception e) {
                     System.err.println("Error in request process:\n" + e);
                     return newFixedLengthResponse(
                             INTERNAL_ERROR,
                             "text/plain",
-                            "I'm sorry Dave, I'm afraid i can't do that. \n¯\\_(ツ)_/¯"
-                    );
+                            "I'm sorry Dave, I'm afraid i can't do that. \n¯\\_(ツ)_/¯");
                 }
             }
             return newFixedLengthResponse(NOT_FOUND, "application/json", "Ticket " + ticketId + " not found. ¯\\_(ツ)_/¯");
@@ -172,16 +147,13 @@ public class Controller implements Serializable {
     }
 
     public Response serveRemoveTicketRequest(IHTTPSession session) {
-
         long ticketId = 0;
-
         Map<String, List<String>> requestParameters = session.getParameters();
 
         if (requestParameters.containsKey(TICKET_ID_PARAMETER_NAME)) {
             List<String> ticketIdParameters = requestParameters.get(TICKET_ID_PARAMETER_NAME);
             String ticketIdParameter = ticketIdParameters.get(0);
-
-
+            
             try {
                 ticketId = Long.parseLong(ticketIdParameter);
             } catch (NumberFormatException nfe) {
@@ -190,14 +162,9 @@ public class Controller implements Serializable {
                         "text/plain",
                         "Request parameter 'ticketId' gotta be a number");
             }
-
             ticketPool.removeTicket(ticketId);
             serializeIt();
-
         }
         return newFixedLengthResponse(OK, "application/json", "Ticket " + ticketId + "has been removed.");
     }
-
-
 }
-
