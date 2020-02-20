34b5a6e1362ad86daad08464f40fd9bd4deed610
vitalispopoff <54749416+vitalispopoff@users.noreply.github.com>
2020-02-20 21:26:45 +0100

    Update Bettor.java

--- src/main/java/programator/types/Bettor.java
+++ src/main/java/programator/types/Bettor.java
@@ -4,26 +4,13 @@ import java.io.Serializable;
 
 public class Bettor implements Serializable {
 
-//    private long bettorId;
     private String bettorLogin;
 
-
-    public Bettor() {
-    }
+    public Bettor() {}
 
     public Bettor(long bettorId, String bettorLogin) {
-
-//        this.bettorId = bettorId;
         this.bettorLogin = bettorLogin;
     }
-
-    /*public long getBettorId() {
-        return bettorId;
-    }*/
-
-    /*public void setBettorId(long bettorId) {
-        this.bettorId = bettorId;
-    }*/
 
     public String getBettorLogin() {
         return bettorLogin;
